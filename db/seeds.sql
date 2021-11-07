1a597da2fe6044f27d7ade9ce7273545030d437f
Misty Whitenton <mistywhitenton@gmail.com>
2021-11-07 16:01:12 -0600

    Reordered database info in numerical order

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,6 +10,6 @@ VALUES ("Head Writer", 150000, 1),
 
 INSERT INTO employees (first_name, last_name, roles_id, manager_id)
 VALUES ("Liz", "Lemon", 1, NULL),
-    ("Jack", "Donaghy", 3, 1)
     ("Tracy", "Jordan", 2, NULL),
-    ("Jenna", "Maroney", 2, NULL);
+    ("Jenna", "Maroney", 2, NULL),
+    ("Jack", "Donaghy", 3, 1);
