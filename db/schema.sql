317cf9b27541ef421db0fcef2a577b99b4ce4226
Misty Whitenton <mistywhitenton@gmail.com>
2021-11-07 15:45:00 -0600

    Updated schema.sql with database structure

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS thirtyrock_db;
+CREATE DATABASE thirtyrock_db;
+
+USE thirtyrock_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(8,2) NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id) 
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    roles_id INT,
+    FOREIGN KEY (roles_id) REFERENCES roles(id),
+    manager_id INT,
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
+    ON DELETE SET NULL
+);
